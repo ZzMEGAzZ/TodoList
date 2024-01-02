51edb320737e07d51c39a4663d27341dc8338c68
ZzMEGAzZ <apisitsangkrajang@gmail.com>
2024-01-02 11:11:19 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,6 @@ dependencies {
     implementation ("com.google.android.material:material:1.11.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    //noinspection LifecycleAnnotationProcessorWithJava8
     annotationProcessor ("androidx.lifecycle:lifecycle-compiler:2.6.2")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation ("androidx.appcompat:appcompat:1.6.1")
@@ -82,7 +81,7 @@ dependencies {
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Import the BoM for the Firebase platform
-    implementation (platform("com.google.firebase:firebase-bom:30.4.1"))
+    implementation (platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation ("com.google.firebase:firebase-database-ktx")
     implementation ("com.google.firebase:firebase-auth:22.3.0")
     testImplementation ("junit:junit:4.13.2")
